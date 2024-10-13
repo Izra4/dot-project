6225f1197f0435c61d30b80f9e36d577bced3a77
izra4 <aryaizra2@gmail.com>
2024-10-13 09:51:02 +0700

    feat(user): add users entity

--- src/models/users.entity.ts
+++ src/models/users.entity.ts
@@ -1,7 +1,12 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
-export class UserEntity {
+export class UsersEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
