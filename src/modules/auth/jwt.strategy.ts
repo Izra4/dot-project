c5dd2b4dd04b0f9e13cda383fface449b7f0a5a7
izra4 <aryaizra2@gmail.com>
2024-10-13 15:46:34 +0700

    feat(jwt): add authentication based on jwt

---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { JwtPayload } from '../../types/jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private usersService: UsersService,
+    private configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload){
+    const user = await this.usersService.findOne(payload.email);
+    if (!user) {
+      return null;
+    }
+    return {
+      id: payload.sub,
+      email: payload.email,
+    };
+  }
+}
